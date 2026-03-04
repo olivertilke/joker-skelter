615a5dd6ac33a23a186a6839d204567c6f358b43
Oliver Tilke <olivertilke@me.com>
2026-03-04 16:35:21 +0100

    Configure Solid Stack for single Heroku PostgreSQL database

---
+++ db/migrate/20260304153028_create_solid_cache_entries.rb
@@ -0,0 +1,25 @@
+class CreateSolidCacheEntries < ActiveRecord::Migration[8.1]
+  def change
+    create_table :solid_cache_entries do |t|
+      t.timestamps
+    end
+  end
+end
+  # For the cache migration:
+def up
+  load Rails.root.join("db/cache_schema.rb")
+end
+def down = raise ActiveRecord::IrreversibleMigration
+
+# For the queue migration:
+def up
+  load Rails.root.join("db/queue_schema.rb")
+end
+def down = raise ActiveRecord::IrreversibleMigration
+
+# For the cable migration:
+def up
+  load Rails.root.join("db/cable_schema.rb")
+end
+def down = raise ActiveRecord::IrreversibleMigration
+
